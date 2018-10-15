2e36396a393232056045ca7db1dabedb4a0a09b1
Ignisor <ignis2497@gmail.com>
2018-10-15 12:19:51 +0300

    trying to use capture_sequence to speed up processing

--- recognition.py
+++ recognition.py
@@ -1,5 +1,7 @@
+import io
 import os
 import logging
+import threading
 
 import face_recognition
 import picamera
@@ -49,8 +51,9 @@ def get_or_create_face_encoding(image_filename):
 
 camera = picamera.PiCamera()
 camera.resolution = (320, 240)
+camera.framerate = 30
 camera.vflip = True
-output = np.empty((240, 320, 3), dtype=np.uint8)
+# output = np.empty((240, 320, 3), dtype=np.uint8)
 
 known_faces_names = []
 known_faces = []
@@ -61,21 +64,100 @@ for filename in os.listdir('known_faces'):
     known_faces.append(get_or_create_face_encoding(filename))
 
 
-while True:
-    logging.info("Capturing image.")
-    camera.capture(output, format="rgb")
+# while True:
+#     logging.info("Capturing image.")
+#     camera.capture(output, format="rgb")
+#
+#     face_locations = face_recognition.face_locations(output)
+#     logging.info("Found {} faces in image.".format(len(face_locations)))
+#     face_encodings = face_recognition.face_encodings(output, face_locations)
+#
+#     for face_encoding in face_encodings:
+#         matches = face_recognition.compare_faces(known_faces, face_encoding)
+#         name = "<Unknown Person>"
+#
+#         if True in matches:
+#             first_match_index = matches.index(True)
+#             name = known_faces_names[first_match_index]
+#
+#             logging.info("Opening door for {}!".format(name))
+#             open_door()
 
-    face_locations = face_recognition.face_locations(output)
-    logging.info("Found {} faces in image.".format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
+# Create a pool of image processors
+done = False
+lock = threading.Lock()
 
-    for face_encoding in face_encodings:
-        matches = face_recognition.compare_faces(known_faces, face_encoding)
-        name = "<Unknown Person>"
+class ImageProcessor(threading.Thread):
+    def __init__(self):
+        super(ImageProcessor, self).__init__()
+        self.stream = io.BytesIO()
+        self.event = threading.Event()
+        self.terminated = False
+        self.start()
 
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_faces_names[first_match_index]
+    def run(self):
+        # This method runs in a separate thread
+        global done
+        while not self.terminated:
+            # Wait for an image to be written to the stream
+            if self.event.wait(1):
+                try:
+                    self.stream.seek(0)
+                    output = np.asarray(bytearray(self.stream.read()), dtype=np.uint8)
 
-            logging.info("Opening door for {}!".format(name))
-            open_door()
+                    face_locations = face_recognition.face_locations(output)
+                    logging.info("Found {} faces in image.".format(len(face_locations)))
+                    face_encodings = face_recognition.face_encodings(output, face_locations)
+
+                    for face_encoding in face_encodings:
+                        matches = face_recognition.compare_faces(known_faces, face_encoding)
+                        name = "<Unknown Person>"
+
+                        if True in matches:
+                            first_match_index = matches.index(True)
+                            name = known_faces_names[first_match_index]
+
+                            logging.info("Opening door for {}!".format(name))
+                            open_door()
+
+                finally:
+                    # Reset the stream and event
+                    self.stream.seek(0)
+                    self.stream.truncate()
+                    self.event.clear()
+                    # Return ourselves to the pool
+                    with lock:
+                        pool.append(self)
+
+
+def streams():
+    while not done:
+        with lock:
+            if pool:
+                processor = pool.pop()
+            else:
+                processor = None
+        if processor:
+            yield processor.stream
+            processor.event.set()
+        else:
+            # When the pool is starved, wait a while for it to refill
+            time.sleep(0.1)
+
+
+with picamera.PiCamera() as camera:
+    pool = [ImageProcessor() for i in range(4)]
+    camera.resolution = (320, 240)
+    camera.framerate = 30
+    camera.vflip = True
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture_sequence(streams(), use_video_port=True)
+
+
+# Shut down the processors in an orderly fashion
+while pool:
+    with lock:
+        processor = pool.pop()
+    processor.terminated = True
+    processor.join()
